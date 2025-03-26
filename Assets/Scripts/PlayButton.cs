689c27df9d8fdb39d5e3130bef9d0d14832b1c80
Bradley <bm17159@ms.wasatch.edu>
2025-03-26 13:55:50 -0600

    Buttons

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class PlayButton : MonoBehaviour
+{
+    private XRBaseInteractable interactable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactable = GetComponent<XRBaseInteractable>();
+        interactable.hoverEntered.AddListener(switchScenes);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void switchScenes(BaseInteractionEventArgs args)
+    {
+        if (args.interactorObject is XRPokeInteractor)
+        {
+            XRPokeInteractor interactor = (XRPokeInteractor)args.interactorObject;
+            SceneManager.LoadScene("Main Game");
+        }
+    }
+}
